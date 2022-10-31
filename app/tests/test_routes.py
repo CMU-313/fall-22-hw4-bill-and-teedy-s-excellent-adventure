08f01592d8853df48ec692f48b2dbae05dbd7270
teresa <tyang218@gmail.com>
2022-10-31 13:01:30 -0400

    changed query string dict keys to strings

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -20,7 +20,7 @@ def test_predict_route(): #what is being returned?
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:"GP", higher:"yes", Mjob: "teacher", Fjob: "health", studytime:8, paid:"yes", failures:0})
+    response = client.get(url, query_string = {'school':"GP", 'higher':"yes", 'Mjob': "teacher", 'Fjob': "health", 'studytime':8, 'paid':"yes", 'failures':0})
 
     assert response.status_code == 200
 
@@ -30,7 +30,7 @@ def test_predict_route2():
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:"GP", higher:"yes", Mjob: "services", Fjob: "other", studytime:4, paid:"yes", failures:1})
+    response = client.get(url, query_string = {'school':"GP", 'higher':"yes", 'Mjob': "services", 'Fjob': "other", 'studytime':4, 'paid':"yes", 'failures':1})
 
     assert response.status_code == 200
 
@@ -40,7 +40,7 @@ def test_predict_route3():
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:"MS", higher:"no", Mjob: "at_home", Fjob: "at_home", studytime:0, paid:"no", failures:3})
+    response = client.get(url, query_string = {'school':"MS", 'higher':"no", 'Mjob': "at_home", 'Fjob': "at_home", 'studytime':0, 'paid':"no", 'failures':3})
 
     assert response.status_code == 200
 
@@ -51,7 +51,7 @@ def test_predict_route4():
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:"hi", higher:"yes", Mjob: "civil", Fjob: "health", studytime:8, paid:"yes", failures:0})
+    response = client.get(url, query_string = {'school':"hi", 'higher':"yes", 'Mjob': "civil", 'Fjob': "health", 'studytime':8, 'paid':"yes", 'failures':0})
 
     assert response.status_code == 404
 
@@ -63,7 +63,7 @@ def test_predict_route5():
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:9, higher:"yes", Mjob: "teacher", Fjob: "health", studytime:8, paid:"yes", failures:0})
+    response = client.get(url, query_string = {'school':9, 'higher':"yes", 'Mjob': "teacher", 'Fjob': "health", 'studytime':8, 'paid':"yes", 'failures':0})
 
     assert response.status_code == 404
 
@@ -75,7 +75,7 @@ def test_predict_route6():
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:"GP", higher:"yes", Mjob: "teacher", Fjob: "health", studytime:8, paid:"yes"})
+    response = client.get(url, query_string = {'school':"GP", 'higher':"yes", 'Mjob': 'teacher', 'Fjob': "health", 'studytime':8, 'paid':"yes"})
 
     assert response.status_code == 404
 
@@ -86,7 +86,7 @@ def test_predict_route6():
     client = app.test_client()
     url = '/predict'
 
-    response = client.get(url, query_string = {school:"GP", higher:"yes"})
+    response = client.get(url, query_string = {'school':"GP", 'higher':"yes"})
 
     assert response.status_code == 404
 
