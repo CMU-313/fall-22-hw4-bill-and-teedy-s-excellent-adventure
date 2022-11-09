f39fa41706961d52a5762ba8b9d78c4db38c6024
alexisaxon <aaxon@andrew.cmu.edu>
2022-11-08 21:45:26 -0500

    corrected input checking for model endpoints

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -18,53 +18,35 @@ def configure_routes(app):
     # modified from https://shzhangji.com/blog/2018/04/07/error-handling-in-restful-api/
     @app.route('/predict')
     def predict():
-
-        class BadRequest(Exception):
-            """Custom exception class to be thrown when local error occurs."""
-            def __init__(self, message, status=404, payload=None):
-                self.message = message
-                self.status = status
-                self.payload = payload
-
-        # modified from https://shzhangji.com/blog/2018/04/07/error-handling-in-restful-api/
-        @app.errorhandler(BadRequest)
-        def handle_bad_request(error):
-            """Catch BadRequest exception globally, serialize into JSON, and respond with 404."""
-            payload = dict(error.payload or ())
-            payload['status'] = error.status
-            payload['message'] = error.message
-            return jsonify(payload), 404
-
-        #use entries from the query string here but could also use json
-        if len(request.args) != 11:
-            raise BadRequest ("incorrect number of fields")
+        if len(request.args) != 9:
+            return 'len', 404
         Fedu = request.args.get('Fedu', type=int)
         if Fedu < 0 or Fedu > 4:
-            raise BadRequest ("bad or no input for Fedu")
+            return 'fedu', 404
         Medu = request.args.get('Medu', type=int)
         if Medu < 0 or Medu > 4:
-            raise BadRequest ("bad or no input for Medu")
+            return 'medu', 404
         Walc = request.args.get('Walc', type=int)
         if Walc < 1 or Walc > 5:
-            raise BadRequest ("bad or no input for Walc")
+            return 'walc', 404
         absences = request.args.get('absences', type=int)
         if absences < 0 or absences > 93:
-            raise BadRequest ("bad or no input for absences")
+            return 'absences', 404
         age = request.args.get('age', type=int)
         if age < 15 or age > 22:
-            raise BadRequest ("bad or no input for age")
+            return 'age', 404
         failures = request.args.get('failures', type=int)
-        if failures < 1 or failures > 5:
-            raise BadRequest ("bad or no input for freetime")
+        if failures < 0 or failures > 5:
+            return 'fails', 404
         freetime = request.args.get('freetime', type=int)
         if freetime < 1 or freetime > 5:
-            raise BadRequest ("bad or no input for freetime")
+            return 'freetime', 404
         goout = request.args.get('goout', type=int)
         if goout < 1 or goout > 5:
-            raise BadRequest ("bad or no input for goout")
+            return 'goout', 404
         health = request.args.get('health', type=int)
         if health < 1 or health > 5:
-            raise BadRequest ("bad or no input for health")
+            return 'health', 404
 
         query_df = pd.DataFrame({ 
                     'Fedu' : pd.Series(Fedu),
@@ -80,5 +62,3 @@ def configure_routes(app):
 
         prediction = rfc.predict(query_df)
         return jsonify(np.ndarray.item(prediction))
-
-# Query String: ?Fedu=1&Medu=3&Walc=4&absences=3&age=17&failures=0&freetime=4&goout=5&health=2
