62f29469828f8f5dc089aa9e7a5a9d76d2ed06b6
coleman-isner-cmu <cisner@andrew.cmu.edu>
2022-11-14 23:06:35 -0500

    added new quality assurance including ProductionData.csv and a new fairnessTesting.py route. Need to fix model_build.ipyn

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.handlers.routes import configure_routes
+from app.handlers.fairnessTesting import configure_routes
 
 app = Flask(__name__)
 
---
+++ app/handlers/fairnessTesting.py
@@ -0,0 +1,51 @@
+import this
+from flask import Flask, jsonify, request
+import joblib
+import pandas as pd
+import numpy as np
+import os
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import preprocessing, neighbors, svm
+from sklearn.ensemble import RandomForestClassifier as rf
+from sklearn.model_selection import train_test_split
+import joblib
+from sklearn.feature_selection import SelectFromModel
+import os
+
+def configure_routes(app):
+
+    this_dir = os.path.dirname(__file__)
+    model_path = os.path.join(this_dir, "model84.pkl")
+    rfc = joblib.load(model_path)
+
+    @app.route('/')
+    def hello():
+        return "try the predict route it is great!"
+
+    # modified from https://shzhangji.com/blog/2018/04/07/error-handling-in-restful-api/
+    @app.route('/predict')
+    def predict():
+        df =pd.read_csv('../../data/ProductionData.csv', sep=',')
+
+        #####################
+        df['qual_student'] = np.where(df['G3']>=15, 1, 0)
+        df.drop(columns=['G3', 'G2', 'G1'], inplace=True) 
+
+        df = pd.get_dummies(df, columns = df.select_dtypes(exclude=["number","bool_"]).columns)
+
+        y = df['qual_student']
+        col = df.columns.difference(['Fedu', 'Medu', 'Walc', 'absences', 'age', 'failures', 'freetime', 'goout', 'health'])
+        X = df[df.columns.difference(col)]
+        print(X.columns)
+
+        result = rfc.score(X, y)
+
+        # print('****Results****')
+        # print("Accuracy: {:.4%}".format(rfc.score(X, y)))
+
+
+        #####################
+
+        return jsonify(np.ndarray.item(result))
